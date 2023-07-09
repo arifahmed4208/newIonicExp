d6b3378843a803a2baa908b70b33edba2e2164df
arifahmed4208 <arifahmed4208@gmail.com>
2023-07-10 00:28:25 +0530

    added status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { CultureService } from './services/culture.service';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,11 @@ import { CultureService } from './services/culture.service';
 })
 export class AppComponent {
   constructor(private culture: CultureService) {}
+
+  async initializeApp() {
+    // ... other initialization code
+
+    await StatusBar.setBackgroundColor({ color: '#FF0000' });
+    await StatusBar.setStyle({ style: Style.Dark });
+  }
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  showQR: boolean = false;
+  showCode: boolean = false;
+  showForm: boolean = false;
+  username: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  showQRCode() {
+    this.showQR = true;
+    this.showCode = false;
+    this.showForm = false;
   }
 
+  showCodeAvatar() {
+    this.showQR = false;
+    this.showCode = true;
+    this.showForm = false;
+  }
+
+  showUsernamePasswordForm() {
+    this.showQR = false;
+    this.showCode = false;
+    this.showForm = true;
+  }
+
+  login() {
+    // Perform login logic using this.username and this.password
+    // Redirect to the desired page or show error message based on login result
+  }
 }
